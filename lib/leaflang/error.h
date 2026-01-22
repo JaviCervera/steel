59a4235a1c885e106590eb17fa9e51aeb3c90a5a
JaviCervera <JaviCervera@users.noreply.github.com>
2026-01-22 23:40:53 +0100

    Remove leaflang's implementation of Error function

--- lib/leaflang/error.h
+++ lib/leaflang/error.h
@@ -4,12 +4,9 @@
 #include "common.h"
 #include "swan/console.hh"
 
-inline void Error(const std::string& message) {
-    swan::console::println(message);
-    exit(-1);
-}
+void Error(const char* message);
 
 
 inline void ErrorEx(const std::string& message, const std::string& file, int line) {
-    Error(file + "(" + swan::strmanip::fromint(line) + "): " + message);
+    Error((file + "(" + swan::strmanip::fromint(line) + "): " + message).c_str());
 }
