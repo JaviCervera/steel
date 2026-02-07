6750cd5372555e5ec78278422c5cb4720a2cd90d
JaviCervera <JaviCervera@users.noreply.github.com>
2026-02-07 15:39:26 +0100

    Document memblock structure for CreateModel

--- src/procedural/model.h
+++ src/procedural/model.h
@@ -15,7 +15,25 @@ struct Model;
 extern "C"
 {
 	/**
-	 * Creates a model from memblock data.
+	 * Creates a model from memblock data. The memblock can contain multiple buffers.
+	 * A buffer is a collection of vertices and indices that will have one material.
+	 * The memblock format is as follows:
+	 * Short: Number of buffers.
+	 * For each buffer:
+	 * Byte: 1 if the buffer has 2 UV sets, 0 otherwise.
+	 * Short: Number of vertices.
+	 * Short: Number of indices.
+	 * For each vertex:
+	 * Float x 3: Position XYZ.
+	 * Float x 3: Normal XYZ.
+	 * Int: Color ARGB.
+	 * Float x 2: UV1.
+	 * Float x 2: UV2 (only if the buffer has 2 UV sets).
+	 * For each index:
+	 * Short: Vertex index.
+	 * Every 3 indices form a triangle.
+	 * The model will be rendered with one material per buffer, so if you want
+	 * different materials on different parts of the model, you should use multiple buffers.
 	 *
 	 * @param data The memblock containing the model geometry data.
 	 * @return A new model, or NULL if creation fails.
