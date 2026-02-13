fd32ffa83d83414fefeecd414cbf81b96f9c3f70
JaviCervera <JaviCervera@users.noreply.github.com>
2026-02-13 08:04:48 +0100

    Fixed render_to_texture example

--- _build/examples/render_to_texture/main.lua
+++ _build/examples/render_to_texture/main.lua
@@ -13,35 +13,20 @@ SetEntityPosition(cam, 0, 0, -2)
 
 local cube = CreateCubeModel()
 
-local x_pos = 0
-local y_pos = 0
-local x_dir = 1
-local y_dir = 1
-
 while IsScreenOpened() and not IsKeyHit(KEY_ESC) do
 	SetCameraAspectRatio(cam, GetScreenWidth() / GetScreenHeight())
 	TurnEntity(cube, 0, 64 * GetDelta(), 0)
-	x_pos = x_pos + x_dir * 64 * GetDelta()
-	y_pos = y_pos + y_dir * 64 * GetDelta()
-	if x_pos <= 0 or x_pos >= 31 then x_pos = Clamp(x_pos, 0, 31); x_dir = x_dir * -1 end
-	if y_pos <= 0 or y_pos >= 31 then y_pos = Clamp(y_pos, 0, 31); y_dir = y_dir * -1 end
-
-	-- Draw 2D pixmap and render to texture
-	Cls(RGB(255, 0, 0))
-	SetColor(RGB(255, 255, 255))
-	DrawRect(x_pos, y_pos, 32, 32)
-	SetColor(RGB(255, 0, 0))
-	DrawLine(x_pos, y_pos, x_pos + 32, y_pos + 32)
-	DrawLine(x_pos + 32, y_pos, x_pos, y_pos + 32)
-	local screen = CreatePixmapFromScreen()
-	local tex = CreateTextureFromPixmap(screen)
-	FreePixmap(screen)
-	FreeTexture(GetMaterialTexture(GetEntityMaterial(cube, 0)))
-	SetMaterialTexture(GetEntityMaterial(cube, 0), tex)
 
 	Cls(RGB(255, 255, 255))
 	DrawScene(cam)
 	SetColor(RGB(0, 0, 0))
 	DrawText(font, tostring(GetScreenFPS()) .. " FPS", 2, 2)
 	RefreshScreen()
+
+	-- Capture screen pixmap and apply to texture
+	local screen = CreatePixmapFromScreen()
+	local tex = CreateTextureFromPixmap(screen)
+	FreePixmap(screen)
+	FreeTexture(GetMaterialTexture(GetEntityMaterial(cube, 0)))
+	SetMaterialTexture(GetEntityMaterial(cube, 0), tex)
 end
